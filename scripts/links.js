9fd63571886af686c664b8e5051271ba0528ec6a
Aaron Delate <aaron@zero-one.io>
2024-02-08 16:54:24 +0200

    removing base url

--- scripts/links.js
+++ scripts/links.js
@@ -19,7 +19,7 @@ async function fetchLinks() {
 }
 
 const displayLinks = (weeks) => {
-  let links = weeks.map(week => `<li>${week.week}: ${week.links.map(link => `<a href="${baseURL}${link.url}">${link.title}</a>`).join(" | ")}</li>`).join("");
+  let links = weeks.map(week => `<li>${week.week}: ${week.links.map(link => `<a href="${link.url}">${link.title}</a>`).join(" | ")}</li>`).join("");
 
   courseLinksEl.innerHTML = links;
 }
